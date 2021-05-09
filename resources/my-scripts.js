d92ada43655450a1d0dc0542a8bc54fd2c26ca4a
samim <samimaftabahmed@gmail.com>
2021-05-09 18:34:21 +0530

    table

--- resources/my-scripts.js
+++ resources/my-scripts.js
@@ -1,3 +1,5 @@
+let myTAble = document.getElementById("myTable");
+
 window.onload = function () {
     getState();
     getDistrict(1);
@@ -27,12 +29,38 @@ function pass() {
 
 function my(response) {
     console.log("Processing response myOK:")
+
     for (let center of response.data.centers) {
+
+        let htmlTableRowElement = document.createElement("tr");
+        let name = document.createElement("td");
+        let address = document.createElement("td");
+        let feeType = document.createElement("td");
+        name.textContent = center.name;
+        address.textContent = center.address;
+        feeType.textContent = center.fee_type;
+
+        htmlTableRowElement.appendChild(name);
+        htmlTableRowElement.appendChild(address);
+        htmlTableRowElement.appendChild(feeType);
+
         for (let session of center.sessions) {
             if (session.available_capacity > 0 && session.min_age_limit < 45) {
                 console.log("vaccine available", session);
+
+                let vaccine = document.createElement("td");
+                let quantity = document.createElement("td");
+                let onDate = document.createElement("td");
+                vaccine.textContent = session.vaccine;
+                quantity.textContent = session.available_capacity;
+                onDate.textContent = session.date;
+
+                htmlTableRowElement.appendChild(vaccine);
+                htmlTableRowElement.appendChild(quantity);
+                htmlTableRowElement.appendChild(onDate);
             }
         }
+        myTAble.appendChild(htmlTableRowElement);
     }
 }
 
